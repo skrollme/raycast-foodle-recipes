cbf778063e70f33d44a40efd65e96a1effa45338
Sebastian Kroll <github@skroll.me>
2025-05-03 15:00:54 +0200

    Added recipe-with command

---
+++ src/recipe-with.tsx
@@ -0,0 +1,23 @@
+import { List } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
+import { useState } from "react";
+import { Searchtype } from "./utils/types";
+import { parseRecipes, fetchItems } from "./utils/fetcher";
+import RecipeItem from "./components/RecipeItem";
+
+export default function Command() {
+  const [searchText, setSearchText] = useState("");
+
+  const { isLoading, data: html, error } = fetchItems(Searchtype.Ingredient, searchText);
+  const recipes = html ? parseRecipes(html) : null;
+
+  if (error) {
+    showFailureToast(error);
+  }
+
+  return (
+    <List isLoading={isLoading} throttle={true} searchText={searchText} onSearchTextChange={setSearchText}>
+      {recipes?.map((recipe) => RecipeItem({ recipe }))}
+    </List>
+  );
+}
